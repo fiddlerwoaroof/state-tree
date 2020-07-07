c21b86c3db4e2ae7a3d471a1397b6338a0194215
fiddlerwoaroof <fiddlerwoaroof@users.noreply.github.com>
2020-07-07 08:32:00 +0000

    chore: reformat

--- src/lens.js
+++ src/lens.js
@@ -1,66 +1,69 @@
-import { Map, fromJS } from 'immutable';
+import { Map, fromJS } from "immutable";
 
-let Symbol = (window['Symbol'] !== undefined) ? window['Symbol'] : x => `_Symbol__${x}`;
+let Symbol =
+  window["Symbol"] !== undefined ? window["Symbol"] : x => `_Symbol__${x}`;
 
-export const fireListeners = Symbol('fireListeners');
+export const fireListeners = Symbol("fireListeners");
 
 const noOpTransform = val => val;
 
 export function lensTransformer(
-    lens,
-    getTransform = noOpTransform,
-    setTransform = noOpTransform
+  lens,
+  getTransform = noOpTransform,
+  setTransform = noOpTransform
 ) {
-    return {
-        get: () => getTransform(lens.get()),
-        set: (val) => lens.set(setTransform(val)),
-        withValue(cb) {
-            return cb(this.get());
-        },
-
-    };
+  return {
+    get: () => getTransform(lens.get()),
+    set: val => lens.set(setTransform(val)),
+    withValue(cb) {
+      return cb(this.get());
+    },
+  };
 }
 
 export function makeLens(key, self) {
-    const createLens = (keyPath) => {
-        return {
-            get() {
-                let result = self._currentState.getIn(keyPath);
-                if (result && result.toJS !== undefined) {
-                    result = result.toJS();
-                }
-                return result;
-            },
-            set(val) {
-                const oldState = self._currentState;
+  const createLens = keyPath => {
+    return {
+      get() {
+        let result = self._currentState.getIn(keyPath);
+        if (result && result.toJS !== undefined) {
+          result = result.toJS();
+        }
+        return result;
+      },
+      set(val) {
+        const oldState = self._currentState;
 
-                if (self._currentState.getIn(keyPath) === undefined) {
-                    for (let x = 0; x < keyPath.length; x++) {
-                        const subPath = keyPath.slice(0, x);
-                        if (self._currentState.getIn(subPath) === undefined && self._currentState.hasIn(subPath)) {
-                            self._currentState = self._currentState.setIn(subPath, Map());
-                        }
-                    }
-                }
-
-                self._currentState = self._currentState.setIn(keyPath, fromJS(val));
-                self[fireListeners](oldState, self._currentState);
-            },
-            lensFor(key) {
-                let subPath = key instanceof Array ? key : [key];
-                return createLens([...keyPath, ...subPath]);
-            },
-
-            withValue(cb, ...rest) {
-                return cb(this.get(), ...rest);
-            },
-
-            swap(cb) {
-                this.set(cb(this.get()));
-                return this.get();
+        if (self._currentState.getIn(keyPath) === undefined) {
+          for (let x = 0; x < keyPath.length; x++) {
+            const subPath = keyPath.slice(0, x);
+            if (
+              self._currentState.getIn(subPath) === undefined &&
+              self._currentState.hasIn(subPath)
+            ) {
+              self._currentState = self._currentState.setIn(subPath, Map());
             }
-        };
-    };
+          }
+        }
 
-    return createLens(key instanceof Array ? key : [key]);
+        self._currentState = self._currentState.setIn(keyPath, fromJS(val));
+        self[fireListeners](oldState, self._currentState);
+      },
+      lensFor(key) {
+        let subPath = key instanceof Array ? key : [key];
+        return createLens([...keyPath, ...subPath]);
+      },
+
+      withValue(cb, ...rest) {
+        return cb(this.get(), ...rest);
+      },
+
+      swap(cb) {
+        this.set(cb(this.get()));
+        return this.get();
+      },
+    };
+  };
+
+  return createLens(key instanceof Array ? key : [key]);
 }
--- src/state_container.js
+++ src/state_container.js
@@ -1,115 +1,114 @@
-import * as R from 'ramda';
-import {
-    Map,
-    fromJS
-} from 'immutable';
-import { makeLens, lensTransformer, fireListeners } from './lens';
+import * as R from "ramda";
+import { Map, fromJS } from "immutable";
+import { makeLens, lensTransformer, fireListeners } from "./lens";
 
 let common_state = {
-    get(key) {
-        return this.lensFor(key).get();
-    },
+  get(key) {
+    return this.lensFor(key).get();
+  },
 
-    set(key, value) {
-        this.lensFor(key).set(value);
-    },
+  set(key, value) {
+    this.lensFor(key).set(value);
+  },
 
-    getState() {
-        return this._currentState.toJS();
-    },
+  getState() {
+    return this._currentState.toJS();
+  },
 
-    lensFor(key) {
-        return makeLens(key instanceof Array ? key : [key], this);
-    },
+  lensFor(key) {
+    return makeLens(key instanceof Array ? key : [key], this);
+  },
 
-    getRecorder() {
-        return new RecordingStateContainer(this);
-    },
+  getRecorder() {
+    return new RecordingStateContainer(this);
+  },
 
-    commit(recorder) {
-        recorder.replay(this);
-    },
+  commit(recorder) {
+    recorder.replay(this);
+  },
 
-    onUpdate(listener) {
-        this.listeners.push(listener);
-        return () => {
-            this.listeners.splice(this.listeners.indexOf(listener), 1);
-        };
-    },
+  onUpdate(listener) {
+    this.listeners.push(listener);
+    return () => {
+      this.listeners.splice(this.listeners.indexOf(listener), 1);
+    };
+  },
 
-    [fireListeners](oldState, newState) {
-        this.listeners.forEach((listener) => {
-            listener(oldState, newState);
-        });
-    },
+  [fireListeners](oldState, newState) {
+    this.listeners.forEach(listener => {
+      listener(oldState, newState);
+    });
+  },
 };
 
 function RecordingStateContainer(container) {
-    this.localStore = Map();
-    this.container = container;
-    this.listeners = [];
-    this.actions = [];
+  this.localStore = Map();
+  this.container = container;
+  this.listeners = [];
+  this.actions = [];
 }
 
-RecordingStateContainer.prototype = Object.assign({
+RecordingStateContainer.prototype = Object.assign(
+  {
     setState(newState) {
-        const oldState = this._currentState;
-        this.localStore = this.localStore.merge(newState);
-        this.actions = [[['setState', this.localStore]]];
-        this[fireListeners](oldState, this._currentState);
+      const oldState = this._currentState;
+      this.localStore = this.localStore.merge(newState);
+      this.actions = [[["setState", this.localStore]]];
+      this[fireListeners](oldState, this._currentState);
     },
 
     get _currentState() {
-        const con = new StateContainer(this.container.getState());
-        this._replay(con);
-        return con._currentState;
+      const con = new StateContainer(this.container.getState());
+      this._replay(con);
+      return con._currentState;
     },
     set _currentState(value) {
-        this.setState(value);
+      this.setState(value);
     },
 
     _replay(container) {
-        R.forEach(
-            R.reduce(
-                (cur, [key, ...args]) => cur[key](...args),
-                container,
-                R.__
-            ),
-            this.actions
-        );
+      R.forEach(
+        R.reduce((cur, [key, ...args]) => cur[key](...args), container, R.__),
+        this.actions
+      );
     },
     replay(container) {
-        if (container !== this.container) {
-            throw new Error("Trying to replay into wrong parent container");
-        }
-        this._replay(container);
-        this.localStore = Map();
-        this.actions = [];
+      if (container !== this.container) {
+        throw new Error("Trying to replay into wrong parent container");
+      }
+      this._replay(container);
+      this.localStore = Map();
+      this.actions = [];
     },
-}, common_state);
+  },
+  common_state
+);
 
 export { lensTransformer };
 
 export default function StateContainer(data = {}) {
-    var state = fromJS(data);
+  var state = fromJS(data);
 
-    return Object.assign({
-        listeners: [],
+  return Object.assign(
+    {
+      listeners: [],
 
-        get _currentState() {
-            return state;
-        },
-        set _currentState(value) {
-            state = value;
-        },
+      get _currentState() {
+        return state;
+      },
+      set _currentState(value) {
+        state = value;
+      },
 
-        /**
-         * setState merges state shallowly
-         * */
-        setState(newState) {
-            const oldState = this._currentState;
-            this._currentState = this._currentState.merge(newState);
-            this[fireListeners](oldState, this._currentState);
-        },
-    }, common_state);
+      /**
+       * setState merges state shallowly
+       * */
+      setState(newState) {
+        const oldState = this._currentState;
+        this._currentState = this._currentState.merge(newState);
+        this[fireListeners](oldState, this._currentState);
+      },
+    },
+    common_state
+  );
 }
